f73ea28fd4f197eb2e0878af88f67edb143029a5
KhylaRedd <kreddick@pursuit.org>
2024-06-26 18:33:12 -0400

    Updates Delete function, adds Validations

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 //import the express
 
 const bookmarksController = require("./controllers/bookmarkcontrollers.js");
- const petController = require("./controllers/petsController")
+ const petController = require("./controllers/petsController");
 //import the bookmarks
 
 
 
 const app = express();
+const cors = require('cors');
 
 
 //middleware
+app.use(cors());
 app.use(express.json());
 //tells our app to accept incoming json 
 app.use('/bookmarks',bookmarksController);
 app.use('/pets',petController);
+
 
 
 
--- controllers/bookmarkcontrollers.js
+++ controllers/bookmarkcontrollers.js
@@ -1,30 +1,58 @@
-const express = require("express");
-const bookmarks = express.Router();
-const bookmarksArray = require("../models/bookmarks.js");
+const express = require('express')
+// Create an instance of our express Router that our server can use to route appropriately
+const bookmarks = express.Router()
 
-//localhost:3400
-bookmarks.get("/", (req, res) => {
-  res.json(bookmarksArray);
-});
+// Import the bookmarks model
+const bookmarksArray = require('../models/bookmarks')
 
-//show routes gets on of the bookmarks 
-// SHOW
-bookmarks.get("/:arrayIndex", (req, res) => {
-  
-  const { arrayIndex } = req.params;
+// Import the validation functions from ../validations/bookmarkValidations.js
+// Middleware functions happen between the request and response. Incase a bookmark is submitted without a name, it will return an error as the response telling the user what they wrong.
+const { checkForNameKey, checkForDotCom } = require('../validations/bookmarkValidations')
 
-  if (bookmarksArray[arrayIndex]) {
-    res.json(bookmarksArray[arrayIndex]);
-  } else {
-    res.status(404).json({ error: "Not Here Friend" });
-  }
-});
 
-//Post
-  bookmarks.post("/", (req, res) => {
-  bookmarksArray.push(req.body);
-  //pushes the bookmarksArray onto the body from the request object
-  res.status(201).json(bookmarksArray[bookmarksArray.length - 1]);
+// Index Route: gets all of the bookmarks
+// localhost:4001/bookmarks/
+bookmarks.get('/', (req, res) => {
+    res.json(bookmarksArray)
+})
+
+// SHOW Route: gets ONE of the bookmarks
+// localhost:4001/bookmarks/0
+bookmarks.get('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params
+    if(bookmarksArray[arrayIndex]){
+        res.status(200).json(bookmarksArray[arrayIndex])
+    } else {
+        res.status(404).json({ error: "Bookmark Not Found" })
+    }
+})
+
+// POST Route: creates a new bookmark and adds it to our array
+// Uses req.body to get information from the request to create a new bookmark
+// localhost:4001/bookmarks/
+bookmarks.post('/', checkForNameKey, checkForDotCom, (req, res) => {
+    bookmarksArray.push(req.body)
+    res.json(bookmarksArray[bookmarksArray.length - 1])
+})
+
+// DELETE Route: delete a bookmark from our bookmarks array
+// localhost:4001/bookmarks/2
+bookmarks.delete('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params
+    if(bookmarksArray[arrayIndex]){
+        const deletedBookmark = bookmarksArray.splice(arrayIndex, 1)
+        res.json(deletedBookmark[0])
+    } else {
+        res.json({ error: "Bookmark Not Found" })
+    }
+})
+
+// PUT Route (update) will update a specific bookmark in our bookmark array to be the body of the request
+// localhost:4001/bookmarks/2
+bookmarks.put("/:arrayIndex", checkForNameKey, checkForDotCom, (req, res) => {
+    const { arrayIndex } = req.params
+    bookmarksArray[arrayIndex] = req.body
+    res.status(200).json(bookmarksArray[arrayIndex])
 })
 
 module.exports = bookmarks;
